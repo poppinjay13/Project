1fe64a4b2817eec8689d626b604cc1ad0832bbfb
techgjules <42571229+garsherrie@users.noreply.github.com>
2018-10-04 22:01:41 +0300

    Update details.php

--- Manager/details.php
+++ Manager/details.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("../config.php");
+include("config.php");
 $tab = 1;
 $count = 0;
 $uid = $_SESSION['Idnum'];
@@ -11,20 +11,26 @@ $row=mysqli_fetch_row($result);
 ?>
 <html>
 	<head>
-		<link href="../assets/css/port.css" type="text/css" rel="stylesheet">
-		<link href="../assets/images/fav.png" rel="icon" type="image/x-icon" />
+		<link href="css/port.css" type="text/css" rel="stylesheet">
+		<link href="images/fav.png" rel="icon" type="image/x-icon" />
 		<title>Manager's details</title>
 	</head>
 	<body>
 	<ul class="navbar">
+
 		<li class="profpic"><img src="../assets/images/pic/<?php echo $uid?>.jpg"></li>
+
 		<li><a href="manager.php"><span>Home</span></a></li>
+
 		<li><a href="#" class="active"><span>My Details</span></a></li>
+
 		<div class="top_right">
+
 		<li><a href="../logout.php" title="logout"><img src="../assets/images/logout.png"></a></li>
+
 	</div>
 	</ul>
-
+	
 	<div class="uhead">
 		<img src="../assets/images/user.png">
 		<div class="uinfo">
@@ -41,9 +47,9 @@ $row=mysqli_fetch_row($result);
 			<input type="text" name="email" value="<?php printf($row[2])?>"><br><br>
 			Phone Number:
 			<input type="text" name="address" value="<?php printf($row[4])?>"><br><br>
-
-
-
+			
+			
+	
 	</div>
 	</body>
 </html>
