7102fdc69bd5fd1e0e16a64e2a7c03646b8f7354
techgjules <42571229+garsherrie@users.noreply.github.com>
2018-10-04 22:03:16 +0300

    Add files via upload

---
+++ Manager/accepted.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include("config.php");
+$idnum= $_GET['IDno'];
+$tenderid = $_SESSION['TenderID'];
+echo $idnum;
+$tab2 = 1;
+$count2 = 0;   
+$sqli = "UPDATE applications SET Status='ACCEPTED' WHERE IDNo='$idnum' && TenderID='$tenderid'";
+$result2 = mysqli_query($conn,$sqli);
+$count2= mysqli_num_rows($result2);
+$row2= mysqli_num_rows($result2);
+            
+if ($conn->query($sqli) === TRUE) {
+    echo "Record updated successfully";
+     header("location:../Project/viewapplicants.php?TENDERID=". $tenderid ."");
+    
+} else {
+    echo "Error updating record: " . $conn->error;
+     header("location:../Project/viewapplicants.php?TENDERID=". $tenderid ."");
+}
+?>
