225b396fcba8181f9ba89938fc62504cfc85eb85
techgjules <42571229+garsherrie@users.noreply.github.com>
2018-10-07 17:31:04 +0300

    Update accepted.php

--- Manager/accepted.php
+++ Manager/accepted.php
@@ -1,22 +1,22 @@
 <?php
 session_start();
-include("config.php");
-$idnum= $_GET['IDno'];
+include("../config.php");
+$idnum= $_GET['TendererID'];
 $tenderid = $_SESSION['TenderID'];
 echo $idnum;
 $tab2 = 1;
 $count2 = 0;   
-$sqli = "UPDATE applications SET Status='ACCEPTED' WHERE IDNo='$idnum' && TenderID='$tenderid'";
+$sqli = "UPDATE applications SET Status='ACCEPTED' WHERE TendererID='$idnum' && TenderID='$tenderid'";
 $result2 = mysqli_query($conn,$sqli);
 $count2= mysqli_num_rows($result2);
 $row2= mysqli_num_rows($result2);
             
 if ($conn->query($sqli) === TRUE) {
     echo "Record updated successfully";
-     header("location:../Project/viewapplicants.php?TENDERID=". $tenderid ."");
+     header("location:../Manager/viewapplicants.php?TENDERID=". $tenderid ."");
     
 } else {
     echo "Error updating record: " . $conn->error;
-     header("location:../Project/viewapplicants.php?TENDERID=". $tenderid ."");
+     header("location:../Manager/viewapplicants.php?TENDERID=". $tenderid ."");
 }
 ?>
